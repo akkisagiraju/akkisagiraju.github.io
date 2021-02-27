7e7d66f4aac6428bc2ba51fdb841af197156578d
akkisagiraju <7601150+akkisagiraju@users.noreply.github.com>
2021-02-27 23:24:01 +0530

    added classes for remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,8 +15,33 @@ module.exports = {
         path: `${__dirname}/src/images`
       }
     },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-classes`,
+            options: {
+              classMap: {
+                'heading[depth=1]': 'text-3xl font-bold my-6 lg:text-4xl dark:text-gray-100',
+                'heading[depth=2]': 'text-2xl font-bold my-6 lg:text-3xl dark:text-gray-100',
+                'heading[depth=3]': 'text-xl font-bold my-6 lg:text-2xl dark:text-gray-100',
+                paragraph: 'text-md my-6 lg:text-lg dark:text-gray-200'
+              }
+            }
+          },
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 600
+            }
+          },
+          'gatsby-remark-prismjs'
+        ]
+      }
+    },
     'gatsby-plugin-postcss',
-    'gatsby-transformer-remark',
+
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp'
   ]
